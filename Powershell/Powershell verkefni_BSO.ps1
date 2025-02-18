75f8546e59eaab7ddebe244ae3877d824fe1b78e
Björn Steinar <42695614+BjornSteinar@users.noreply.github.com>
2025-02-18 14:05:39 +0000

    Add files via upload

---
+++ Powershell/Powershell verkefni_BSO.ps1
@@ -0,0 +1,18 @@
+﻿Write-Output 'Sláðu inn notendanafn:'
+$username = Read-Host
+Write-Output 'Sláðu inn fullt nafn:'
+$fullname = Read-Host
+Write-Output 'Sláðu inn lykilorð:'
+$password = Read-Host -AsSecureString
+$description = 'This is a test User'
+
+New-LocalUser -Name $username -Password $password -Description $description -FullName $fullname
+
+$groupname = 'Test Users'
+New-LocalGroup -Name $groupname
+
+Add-LocalGroupMember -Group $groupname -Member $username
+
+Get-LocalGroup $groupname
+
+Get-LocalGroupMember -Group $groupname -Member $username
